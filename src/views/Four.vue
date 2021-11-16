7852b4dc64f9be438208c2531d750f1515ab72e3
karend2105 <liendanld@gmail.com>
2021-11-16 03:23:59 -0800

    designed the 404 page

--- src/views/Four.vue
+++ src/views/Four.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="">
-      <h1>404</h1>
+  <div class="404page">
+    <img src="../assets/backgrounds/page404.png" alt="Page404Image">
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, we could not find the page that you were looking for.</p>
+      <router-link class="btn btn-outline-primary" :to="{name:'Home'}">Back Home</router-link>
   </div>
 </template>
 
@@ -11,5 +14,17 @@ name:'Four'
 </script>
 
 <style>
+body {
+  background-color: rgb(245, 211, 196);
+  color: rgb(0, 0, 0)
+}
+/* img {
+   display: block;
+   margin-left: auto;
+   margin-right: auto;
+   margin-top: 50px;
+   width:50%;
+   height: auto; 
+} */
 
 </style>

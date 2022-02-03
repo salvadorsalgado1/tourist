2d6aa790e8df3110449541083db39fc700d48087
salvadorsalgado1 <salvadorsalgado5694@gmail.com>
2022-02-02 20:58:13 -0800

    remove spinner from homepage

--- src/components/Typed.vue
+++ src/components/Typed.vue
@@ -3,19 +3,14 @@
     <img class="we-travel-logo mb-1" src="../assets/logos/we-travel/logo-we-travel-sm-transparent-sm-sm.png" alt=""/>
     <p class = "display-4 type-it" id="element"></p>
     <router-link class="btn btn-outline-primary" :to="{name:'Home'}" >Get Started</router-link>
-    <Spinner :start="!spin" />
   </div>
 </template>
 
 <script>
 import TypeIt from 'typeit'
-import Spinner from '../components/Spinner'
+ 
 
 export default {
-     components:{Spinner},
-    data: () => ({
-        spin: false
-    }),
     data(){
         return{
             string:this.sentence

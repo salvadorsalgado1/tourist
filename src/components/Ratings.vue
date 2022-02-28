142e9fd0e09be70ecaac7bcb125369daa3d81490
karend2105 <liendanld@gmail.com>
2022-02-28 10:45:34 -0800

    rating added

--- src/components/Ratings.vue
+++ src/components/Ratings.vue
@@ -3,8 +3,8 @@
         <div class="card shadow">
             <div class="card-body">
                 <h2>Achievements and Ratings</h2>
-                <p>Ratings: <!--TODO-->4.7</p>
-                <p>Reviews: <!--TODO-->5</p>
+                <p>Ratings: <!--TODO--4.7--> {{ getRating }} </p>
+                <p>Reviews: <!--TODO--5--> {{ getReviews }}</p>
             </div>
         </div>
     </div>
@@ -14,15 +14,19 @@
 export default {
 computed:{
     getRatings(){
-        let rating = this.$store.state.reviews.reviews;
+        let rating = this.$store.state.reviews.reviews; //what is this code do????????
         //TODO
-
-
-
-
-
-
+        var avg;  //hold sum and result after compute avg
+        var i=0;
+            for(i; i<userReviews.reviews.length; i++) {
+            avg += userReviews.reviews[i].stars;
+        };
+        rating = avg/i;
         return rating
+    },
+    getReviews(){
+       
+        return userReviews.reviews.length;
     }
 }
 }

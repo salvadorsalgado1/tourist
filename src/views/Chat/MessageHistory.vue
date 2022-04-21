ac579357bfea2b44b606a5b5dd6983be0b433a5e
salvadorsalgado1 <salvadorsalgado5694@gmail.com>
2022-04-21 00:02:00 -0700

    add select bootstrap css messaging

--- src/views/Chat/MessageHistory.vue
+++ src/views/Chat/MessageHistory.vue
@@ -1,54 +1,44 @@
 <template>
   <div class="inbox_people">
-           
-          <div class="inbox_chat">
-            <div class="chat_list active_chat">
-              <div class="chat_people">
-                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" class="img-mes" alt="sunil"> </div>
-                <div class="chat_ib">
-                  <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                  <p>Test, which is a new approach to have all solutions 
-                    astrology under one roof.</p>
-                </div>
-              </div>
-            </div>
-            <div class="chat_list">
-              <div class="chat_people">
-                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" class="img-mes" alt="sunil"> </div>
-                <div class="chat_ib">
-                  <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                  <p>Test, which is a new approach to have all solutions 
-                    astrology under one roof.</p>
-                </div>
-              </div>
-            </div>
-            <div class="chat_list">
-              <div class="chat_people">
-                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" class="img-mes" alt="sunil"> </div>
-                <div class="chat_ib">
-                  <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                  <p>Test, which is a new approach to have all solutions 
-                    astrology under one roof.</p>
-                </div>
-              </div>
-            </div>
-            <div class="chat_list list-group" id="list-tab" role="tablist">
-              <div class="chat_people">
-                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" class="img-mes" alt="sunil"> </div>
-                <div class="chat_ib">
-                  <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                  <p>Test, which is a new approach to have all solutions 
-                    astrology under one roof.</p>
-                </div>
-              </div>
-            </div>
+    <div class="inbox_chat list-group" id="list-tab" role="tablist">
+      <a v-for="message in history" :key="message.id" class="chat_list active_chat list-group-item list-group-item-action" :id="`list-${message.id}-list`" data-bs-toggle="list" :href="`#list-${message.id}`" role="tab" :aria-controls="`list-${message.id}`">
+        <div class="chat_people">
+          <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" class="img-mes" alt="sunil"> </div>
+          <div class="chat_ib">
+            <h5>{{message.name}} <span class="chat_date">{{message.date}}</span></h5>
+            <p>{{message.preview}}</p>
           </div>
         </div>
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+data(){
+  return{
+    history:[
+      {
+        id:'1',
+        name:'Peter Jackson', 
+        date:'April 16, 2022', 
+        preview:'Apples are grown in such quantities as to entitle the county.'
+      }, 
+      {
+        id:'2',
+        name:'Maria Jens', 
+        date:'April 19, 2022', 
+        preview:'Apples are grown in such quantities as to entitle the county.'
+      }, 
+      {
+        id:'3',
+        name:'Lauren Jackson', 
+        date:'April 13, 2022', 
+        preview:'Apples are grown in such quantities as to entitle the county.'
+      }, ]
+  }
+}
 }
 </script>
 
--- src/views/Chat/Messages.vue
+++ src/views/Chat/Messages.vue
@@ -1,12 +1,47 @@
 <template>
-  <div class="mesgs">
-          <div class="msg_history">
+  <div class="mesgs tab-content" id="nav-tabContent">
+          <div class="msg_history tab-pane fade show active" id="list-1" role="tabpanel" aria-labelledby="list-1-list">
             <div class="incoming_msg">
               <div class="incoming_msg_img"> <img class="img-mes" src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
               <div class="received_msg">
                 <div class="received_withd_msg">
-                  <p>Test which is a new approach to have all
-                    solutions</p>
+                  <p>First tiam leo lacus, vehicula sit amet tellus vel, maximus bibendum augue.</p>
+                  <span class="time_date"> 11:01 AM    |    June 9</span></div>
+              </div>
+            </div>
+            <div class="outgoing_msg">
+              <div class="sent_msg">
+                <p>Mauris ut orci malesuada, pulvinar libero sed,</p>
+                <span class="time_date"> 11:01 AM    |    June 9</span> </div>
+            </div>
+            <div class="incoming_msg">
+              <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" class="img-mes" alt="sunil"> </div>
+              <div class="received_msg">
+                <div class="received_withd_msg">
+                  <p>Test, which is a new approach to have</p>
+                  <span class="time_date"> 11:01 AM    |    Yesterday</span></div>
+              </div>
+            </div>
+            <div class="outgoing_msg">
+              <div class="sent_msg">
+                <p>massa fermentum, in rhoncus dolor maximus. Ut vel vehicula eros. Donec ornare finibus urna vitae mattis. Vivamus condimentum, massa in molestie egestas, neque libero porta metus, nec scelerisque ex lacus convallis nunc. Mauris ut orci malesuada, pulvinar libero sed, placerat ipsum. Ut tempor sapien volutpat, feugiat lorem nec, lobortis arcu. Etiam leo lacus, vehicula sit amet tellus vel, maximus bibendum augue.</p>
+                <span class="time_date"> 11:01 AM    |    Today</span> </div>
+            </div>
+            <div class="incoming_msg">
+              <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" class="img-mes" alt="sunil"> </div>
+              <div class="received_msg">
+                <div class="received_withd_msg">
+                  <p>entum elementum. Nam in eleifend diam, vitae interdum risus.</p>
+                  <span class="time_date"> 11:01 AM    |    Today</span></div>
+              </div>
+            </div>
+          </div>
+          <div class="msg_history tab-pane fade show" id="list-2" role="tabpanel" aria-labelledby="list-2-list">
+            <div class="incoming_msg">
+              <div class="incoming_msg_img"> <img class="img-mes" src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+              <div class="received_msg">
+                <div class="received_withd_msg">
+                  <p>Cras feugiat mollis purus, vel sagittis orci vulputate eget. Donec vitae condimentum ipsum. Fusce suscipit a sem et vehicula.</p>
                   <span class="time_date"> 11:01 AM    |    June 9</span></div>
               </div>
             </div>
@@ -26,7 +61,7 @@
             </div>
             <div class="outgoing_msg">
               <div class="sent_msg">
-                <p>Apollo University, Delhi, India Test</p>
+                <p>Cras feugiat mollis purus, vel sagittis orci vulputate eget. Donec vitae condimentum ipsum. Fusce suscipit a sem et vehicula.</p>
                 <span class="time_date"> 11:01 AM    |    Today</span> </div>
             </div>
             <div class="incoming_msg">
@@ -36,6 +71,42 @@
                   <p>We work directly with our designers and suppliers,
                     and sell direct to you, which means quality, exclusive
                     products, at a price anyone can afford.</p>
+                  <span class="time_date"> 11:01 AM    |    Today</span></div>
+              </div>
+            </div>
+          </div>
+          <div class="msg_history tab-pane fade show" id="list-3" role="tabpanel" aria-labelledby="list-3-list">
+            <div class="incoming_msg">
+              <div class="incoming_msg_img"> <img class="img-mes" src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+              <div class="received_msg">
+                <div class="received_withd_msg">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non sapien dui. Nullam in magna diam. Vivamus est metus, ornare sed elementum vel, vehicula in justo.</p>
+                  <span class="time_date"> 11:01 AM    |    June 9</span></div>
+              </div>
+            </div>
+            <div class="outgoing_msg">
+              <div class="sent_msg">
+                <p>Cras feugiat mollis purus, vel sagittis orci vulputate eget. Donec vitae condimentum ipsum. Fusce suscipit a sem et vehicula.</p>
+                <span class="time_date"> 11:01 AM    |    June 9</span> </div>
+            </div>
+            <div class="incoming_msg">
+              <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" class="img-mes" alt="sunil"> </div>
+              <div class="received_msg">
+                <div class="received_withd_msg">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non sapien dui. Nullam in magna diam. Vivamus est metus.</p>
+                  <span class="time_date"> 11:01 AM    |    Yesterday</span></div>
+              </div>
+            </div>
+            <div class="outgoing_msg">
+              <div class="sent_msg">
+                <p>pulvinar libero sed, placerat ipsum. Ut tempor sapien volutpat, feugiat lorem nec, lobortis arcu. Etiam leo lacus, vehicula sit amet tellus vel, maximus bibendum augue.</p>
+                <span class="time_date"> 11:01 AM    |    Today</span> </div>
+            </div>
+            <div class="incoming_msg">
+              <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" class="img-mes" alt="sunil"> </div>
+              <div class="received_msg">
+                <div class="received_withd_msg">
+                  <p> Fusce suscipit a sem et vehicula. Nunc in nisi vitae leo pharetra commodo in sed risus. Duis finibus tellus non lobortis mattis. Integer pretium feugiat mauris, et porta dolor imperdiet eu.</p>
                   <span class="time_date"> 11:01 AM    |    Today</span></div>
               </div>
             </div>
@@ -54,7 +125,40 @@
 export default {
 data(){
   return{
-    message:''
+     messages:[
+      {
+        id:'1',
+        name:'Peter Jackson',
+        conversation:[
+          {
+            user:1, 
+            date:'11:01AM', 
+            preview:'Apples are grown in such quantities as to entitle the county.'
+          },
+          {
+            user:2, 
+            date:'11:02AM', 
+            preview2:'There is a great variety of produce, but the principal crops are Indian corn, wheat, oats, hay, potatoes, apples and tobacco.'
+          },
+          {
+            user:1, 
+            date:'11:03AM', 
+            preview2:'There is a great variety of produce, but the principal crops are Indian corn, wheat, oats, hay, potatoes, apples and tobacco.'
+          },
+          {
+            user:1, 
+            date:'11:03AM', 
+            preview2:'There is a great amount of fruit and apples.'
+          },
+          {
+            user:2, 
+            date:'11:14AM', 
+            preview2:'Of the former the chief kinds are pears, apples, plums, apricots, peaches'
+          }
+     
+        ],
+      }
+     ] 
   }
 }
 }
